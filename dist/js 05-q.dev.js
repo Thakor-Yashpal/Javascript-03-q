7b5435ce547b0f77ad7df76f7d4fc9b45e18f9ab
yashpal <75574310+nova7665@users.noreply.github.com>
2022-09-16 01:13:24 +0530

    new questions
    change the color of html and css oen

---
+++ dist/js 05-q.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log(" MY name is yashpal");
+var a = "💤 ";
+console.log(a);
+var b = " 👋";
+console.log(b); //console.log("num")// what is num ?
+
+var c = 33; // a contains 33
+
+console.log(c);
+var e = "nova";
+console.log(e);
+var arr1 = [1, 2, 3, 42, 2423423, 423, 423, 42342343, 3];
+console.log(arr1);
+//# sourceMappingURL=js 05-q.dev.js.map
---
+++ js 05-q.js
@@ -0,0 +1,18 @@
+console.log(" MY name is yashpal")
+var a = "💤 "
+console.log(a)
+
+let b = " 👋"
+console.log(b)
+
+//console.log("num")// what is num ?
+let c = 33 // a contains 33
+console.log(c)
+
+let e = "nova"
+console.log(e)
+
+
+const arr1 = [1, 2, 3, 42, 2423423, 423, 423, 42342343, 3]
+
+console.log(arr1)
